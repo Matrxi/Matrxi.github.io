c9c07c784ed158b75d2d36ae606f25acd7158570
Rishabh Aggarwal <rishabhaggarwal005@gmail.com>
2017-12-16 23:55:58 +0530

    Add files via upload
    JavaScript Files

---
+++ JavaScript/1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html5>
+<html xml:lang="en" lang="en">
+  <head>
+    <title>Today's Date</title>
+    <meta http-equiv="content-type"
+        content="text/html; charset=utf-8"/>
+  </head>
+      <?php
+      echo date(‘H:i, jS F’);
+      ?>
+</html
