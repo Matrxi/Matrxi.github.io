6ddbc7eeb93cc779c309e34f5cb265c0c6373d6a
Rishabh Aggarwal <rishabhaggarwal005@gmail.com>
2017-12-17 15:05:42 +0530

    Update init.js

--- js/init.js
+++ js/init.js
@@ -23,12 +23,12 @@
 
    // main directions
    map = new GMaps({
-      el: '#map', lat: 14.553533, lng: 121.052124, zoom: 14, zoomControl : true,
+      el: '#map', lat: 30.273423, lng: 77.999822, zoom: 14, zoomControl : true,
       zoomControlOpt: { style : 'SMALL', position: 'TOP_LEFT' }, panControl : false, scrollwheel: false
    });
 
    // add address markers
-   map.addMarker({ lat: 14.552891, lng: 121.051815, title: 'Eco Tower',
+   map.addMarker({ lat: 30.273423, lng: 77.999822, title: 'Eco Tower',
    infoWindow: { content: '<p>You can add your address 1 here</p>' } });
 
 /*----------------------------------------------------*/
@@ -59,7 +59,7 @@
                $('#image-loader').fadeOut();
                $('#message-warning').hide();
                $('#contactForm').fadeOut();
-               $('#message-success').fadeIn();   
+               $('#message-success').fadeIn();
             }
             // There was an error
             else {
@@ -78,11 +78,3 @@
 
 
 });
-
-
-
-
-
-
-
-
